5ddc64f4a97cbdc5c1ab2d42e0bf075f02be58ad
Edwin <mredwinrodriguez@gmail.com>
2023-07-09 19:50:12 -0400

    header

---
+++ client/src/components/Header.js
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import {
+    Navbar,
+    NavbarBrand,
+    Collapse,
+    NavbarToggler,
+    Nav,
+    NavItem
+} from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+import NucampLogo from '../logo.svg';
+
+
+const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    return (
+        <Navbar dark color='primary' sticky='top' expand='md'>
+            <NavbarBrand className='ms-5' href='/'>
+                <img src={NucampLogo} alt='nucamp logo' className='float-start' />
+                <h1 className='mt-1'>My Office Pool</h1>
+            </NavbarBrand>
+            
+            <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
+            <Collapse isOpen={menuOpen} navbar>
+                <Nav className='ms-auto' navbar>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/'>
+                            <i className='fa fa-home fa-lg' /> Home
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/mygames'>
+                            <i className='fa fa-list fa-lg' /> My Games
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/findgames'>
+                            <i className='fa fa-info fa-lg' /> Find Games
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+            </Collapse>
+        </Navbar>
+    );
+};
+
+export default Header;
